a49078d3dca187d1e82ee230942b65046778dbcc
pillipa <05may882@gmail.com>
2024-12-01 05:39:40 +0900

    정렬 함수 제작 및 기본 수정

--- birthday_program/birthday_jwerkbuger.c
+++ birthday_program/birthday_jwerkbuger.c
@@ -2,6 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX 100 // 임의 선정
+
+// 정렬 함수 제작
+struct person {
+    int month;
+    int day;
+    char name[20];
+    char etc[100];
+};
+int compare_name(const void* a, const void* b) {
+    struct person* personA = (struct person*)a;
+    struct person* personB = (struct person*)b;
+    return strcmp(personA->name, personB->name);
+}
+int compare_date(const void* a, const void* b) {
+    struct person* personA = (struct person*)a;
+    struct person* personB = (struct person*)b;
+
+    // 월
+    if (personA->month != personB->month) {
+        return personA->month - personB->month;
+    }
+    // 일
+    return personA->day - personB->day;
+}
 
 // 기본 입력 제작
 int main() {
@@ -12,6 +37,7 @@ int main() {
         exit(1);
     }
 
+    struct person people[MAX];
     int cnt = 0;
     do {
         int choice = 0;
@@ -25,28 +51,26 @@ int main() {
             break;
         }
 
-        int month = 0, day = 0;
-        char name[20];
-        char etc[100];
-
         switch (choice) {
         case 1:
-            cnt++;
+            if (cnt >= MAX) {
+                printf("정원 초과 입니다.\n");
+                break;
+            }
 
             printf("양식에 맞춰 입력해주세요.\n");
             printf("날짜(00/00) : ");
-            scanf("%d/%d", &month, &day);
-            fprintf(fp, "%d/%d ", month, day);
+            scanf("%d/%d", &people[cnt].month, &people[cnt].day);
 
             printf("이름 : ");
-            scanf("%s", name);
-            fprintf(fp, "%s ", name);
+            scanf("%s", people[cnt].name);
 
             printf("기타(비고) : ");
-            scanf("%s", etc);
-            fprintf(fp, "%s\n", etc);
+            scanf("%s", people[cnt].etc);
+            fprintf(fp, "%d/%d %s %s\n", people[cnt].month, people[cnt].day, people[cnt].name, people[cnt].etc);
 
             printf("저장이 완료되었습니다.\n");
+            cnt++;
             break; 
         }
     } while (1);
