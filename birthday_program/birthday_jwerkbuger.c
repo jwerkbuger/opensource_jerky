8a8b4f45da802ff7e044179bba9d6e3275e97f1e
pillipa <05may882@gmail.com>
2024-12-01 06:26:23 +0900

    충돌부분 수정

--- birthday_program/birthday_jwerkbuger.c
+++ birthday_program/birthday_jwerkbuger.c
@@ -39,6 +39,7 @@ int main() {
 
     struct person people[MAX];
     int cnt = 0;
+    char search[10];
     do {
         int choice = 0;
 
@@ -72,6 +73,34 @@ int main() {
             printf("저장이 완료되었습니다.\n");
             cnt++;
             break; 
+        case 2:
+            printf("정렬하실 방법(추가/이름/날짜) 혹은 이름을 입력해주세요.\n");
+            scanf("%s", search);
+            if (search == "추가") {
+                printf("생일 목록을 추가순으로 출력합니다.\n");
+                for (int i = 0; i < cnt; i++) {
+                    printf("%02d/%02d %s %s\n", people[cnt].month, people[cnt].day, people[cnt].name, people[cnt].etc);
+                }
+            }
+            else if (search == "이름") {
+                printf("생일 목록을 이름순으로 출력합니다.\n");
+                qsort(people, cnt, sizeof(struct person), compare_name);
+                for (int i = 0; i < cnt; i++) {
+                    printf("%02d/%02d %s %s\n", people[cnt].month, people[cnt].day, people[cnt].name, people[cnt].etc);
+                }
+            }
+            else if (search == "날짜") {
+                printf("생일 목록을 날짜순으로 출력합니다.\n");
+                qsort(people, cnt, sizeof(struct person), compare_date);
+                for (int i = 0; i < cnt; i++) {
+                    printf("%02d/%02d %s %s\n", people[cnt].month, people[cnt].day, people[cnt].name, people[cnt].etc);
+                }
+            }
+
+            break;
+        case 3:
+            printf("수정 기능은 아직 지원하지 않습니다.\n");
+            break;
         }
     } while (1);
 
